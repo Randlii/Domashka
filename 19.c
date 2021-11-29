a317665391da124ddefdb36abf7040ebc6245348
Randlii <93086288+Randlii@users.noreply.github.com>
2021-11-30 00:52:27 +0300

    Update 19.c

--- 19.c
+++ 19.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-int main() // не работает на вижл, не копируйте пока!
+#include <iostream>
+int main() 
 {
+	setlocale(LC_ALL, "Rus");
 	char s[100];
 	int i = 0, c = 0, d;
 
 	printf("Введите строку: ");
 	gets_s(s);
-	for(int i1 = 0, l; s[i1] != '\0';i1++)
+	for (int i1 = 0, l; s[i1] != '\0'; i1++)
 	{
-        if(s[i1+1] == ' ' || s[i1+1] == ',')
-        {
-        	l = i1+1;
-        	while(s[l])
-        	{
-        		s[l] = s[l+1];
-        		l++;
-        	}
-        	i1--;
-        }
-       	s[i1] = (char)tolower(s[i1]);
-    }
-    c = strlen(s) -2;
+		if (s[i1 + 1] == ' ' || s[i1 + 1] == ',')
+		{
+			l = i1 + 1;
+			while (s[l])
+			{
+				s[l] = s[l + 1];
+				l++;
+			}
+			i1--;
+		}
+		s[i1] = (char)tolower(s[i1]);
+	}
+	c = strlen(s) - 1;
 	d = c;
-	while(s[i] == s[c]) 
+	while (s[i] == s[c])
 	{
 		i++;
 		c--;
-		if(i == d)
+		if (i == d)
 		{
 			printf("Это паллиндром!!!");
 			return 0;
