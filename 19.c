0d818b93945fdd7774570361af05bfc4173310c8
Randlii <93086288+Randlii@users.noreply.github.com>
2021-11-29 20:14:20 +0300

    f

---
+++ 19.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+int main()
+{
+	char s[100];
+	int i = 0, c = 0, d;
+
+	printf("Введите строку: ");
+	gets_s(s);
+	for(int i1 = 0, l; s[i1] != '\0';i1++)
+	{
+        if(s[i1+1] == ' ' || s[i1+1] == ',')
+        {
+        	l = i1+1;
+        	while(s[l])
+        	{
+        		s[l] = s[l+1];
+        		l++;
+        	}
+        	i1--;
+        }
+       	s[i1] = (char)tolower(s[i1]);
+    }
+    c = strlen(s) -2;
+	d = c;
+	while(s[i] == s[c]) 
+	{
+		i++;
+		c--;
+		if(i == d)
+		{
+			printf("Это паллиндром!!!");
+			return 0;
+		}
+	}
+	printf("Это не паллиндром:(");
+}
