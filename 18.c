31a7ad32d07d7c1f751937cac42c82d44e31395f
Randlii <93086288+Randlii@users.noreply.github.com>
2021-11-29 19:19:29 +0300

    new

---
+++ 18.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main()
+{
+	char s[100];
+	char del[100];
+	int i = 0, c = 0, d = 0,l= 0;
+
+	printf("Введите строку: ");
+	gets_s(s);
+	printf("Введите слово которое нужно удалить: ");
+	scanf_s("%s", del);
+	while(s[i] != '\0') {
+		while(s[i] == del[c])
+		{ 
+			if(s[i+1] == ' ' || s[i+1] == '\0' || s[i+1] == '\n')
+			{
+				d = i - c - 1;
+				l = i;
+				for(int d = i -c -1; s[l];)
+					s[d++] = s[++l];
+				s[l] = '\0';
+			}
+			i++;
+			c++;
+		}
+		c = 0;
+		i++;
+
+	}
+	puts(s);
+}
