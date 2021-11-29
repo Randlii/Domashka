793ea37a74ba6cd8298dcce3663291c8c0df31da
Randlii <93086288+Randlii@users.noreply.github.com>
2021-11-30 00:46:04 +0300

    Update 18.c

--- 18.c
+++ 18.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
-int main() // не работает на вижл, не копируйте пока
+#include <iostream>
+
+int main() 
 {
+	setlocale(LC_ALL, "Rus");
 	char s[100];
 	char del[100];
-	int i = 0, c = 0, d = 0,l= 0;
+	int i = 0, c = 0, d = 0, l = 0;
 
 	printf("Введите строку: ");
 	gets_s(s);
 	printf("Введите слово которое нужно удалить: ");
-	scanf_s("%s", del);
-	while(s[i] != '\0') {
-		while(s[i] == del[c])
-		{ 
-			if(s[i+1] == ' ' || s[i+1] == '\0' || s[i+1] == '\n')
+	gets_s(del);
+	while (s[i] != '\0') {
+		while (s[i] == del[c])
+		{
+			if (s[i + 1] == ' ' || s[i + 1] == '\0' || s[i + 1] == '\n')
 			{
 				d = i - c - 1;
 				l = i;
-				for(int d = i -c -1; s[l];)
+				for (int d = i - c - 1; s[l] == '\0';)
 					s[d++] = s[++l];
 				s[l] = '\0';
 				i = 0;
